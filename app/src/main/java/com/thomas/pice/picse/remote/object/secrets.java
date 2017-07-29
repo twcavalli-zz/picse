eb3727b2e250c4f7534538a77e22d4190b5da621
Thomas Cavalli <thomas.cavalli@gmail.com>
2017-07-29 11:55:00 -0300

    Update secrets.java

--- app/src/main/java/com/thomas/pice/picse/remote/object/secrets.java
+++ app/src/main/java/com/thomas/pice/picse/remote/object/secrets.java
@@ -8,8 +8,8 @@ import android.util.Base64;
 
 public class Secrets {
     private String ENDPOINT = "https://api.twitter.com/";
-    private String CONSUMER_KEY = "hdOYGwHuKWqwF5lp2Uqetocxs";
-    private String CONSUMER_SECRET = "47yLvxvdCdsvJPzpAJAK8gXWip6LjThNQypnvYE4cb1QzoPixo";
+    private String CONSUMER_KEY = "YOUR_CONSUMER_KEY";
+    private String CONSUMER_SECRET = "YOUR_CONSUMER_SECRET";
     private String CONTENT_TYPE = "application/x-www-form-urlencoded;charset=UTF-8";
     private String GRANT_TYPE = "client_credentials";
     private String ENCODED_BEARER = "Basic " + Base64.encodeToString((CONSUMER_KEY + ":" + CONSUMER_SECRET).getBytes(),Base64.NO_WRAP);
